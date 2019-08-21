2ff2cd210a4683b38ec4758789bd3dd70574115b
Johan Uribe <juribe@ETs-MacBook-Pro.local>
2019-08-21 09:10:25 -0400

    removed flake8 from travis.yml

--- app/core/models.py
+++ app/core/models.py
@@ -14,15 +14,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
-    
+
     def create_superuser(self, email, password):
         """create and saves a new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
 
 
