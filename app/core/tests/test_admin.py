8bc7ed1fdb682d7dceca3b284dd74214f9d3fa21
Johan Uribe <juribe@ETs-MacBook-Pro.local>
2019-08-21 22:17:17 -0400

    configured postgres db

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,25 +17,25 @@ class AdminSiteTests(TestCase):
             password='test123',
             name='test user full name'
         )
-        
+
     def test_user_listed(self):
         """test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-        
+
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-        
+
     def test_user_change_page(self):
         """Test that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-        
+
         self.assertEqual(res.status_code, 200)
-        
+
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-        
+
         self.assertEqual(res.status_code, 200)
