2e5bcbb344c9bb285747c220e2bdba56cc715e25
Johan Uribe <johanandresuribe@gmail.com>
2019-08-28 14:41:16 -0400

    fixed typos in ingredients tests

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -28,12 +28,14 @@ class PublicIngredientsApiTest(TestCase):
 
 class PrivateIngredientsApiTest(TestCase):
     """Test the private ingredients API"""
-    self.client = APIClient()
-    self.user = get_user_model().objects.create_user(
-        'test@gmail.com',
-        'test123'
-    )
-    self.client.force_authenticate(self.user)
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@gmail.com',
+            'test123'
+        )
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_ingredients_list(self):
         """Test retrieving a list of ingredients"""
@@ -49,7 +51,7 @@ class PrivateIngredientsApiTest(TestCase):
 
     def test_ingredients_limited_to_user(self):
         """Test that ingredients for the authenticated user are returned"""
-        user2 = get_user_model.objects().create_user(
+        user2 = get_user_model().objects.create_user(
             'other@gmail.com',
             "testpass"
         )
@@ -61,4 +63,4 @@ class PrivateIngredientsApiTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0], ingredient.name)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
